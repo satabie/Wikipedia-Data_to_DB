6d1c672f9d004f933c189cc670a98322b21eeadb
satabie <0426193@gmail.com>
2022-12-05 09:45:29 +0900

    update: all completed

--- add_data_to_DB.py
+++ add_data_to_DB.py
@@ -19,7 +19,7 @@ DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_USER = "postgres"
 DB_PASS = "postgres"
-DB_NAME = "test3"
+DB_NAME = "wikipedia_featured_articles"
 
 
 def main():
--- extractFA.py
+++ extractFA.py
@@ -6,10 +6,11 @@ import codecs
 import os
 
 os.mkdir('Topic')
-with open("wiki_feature_articles.txt", "r", encoding="utf-8", errors="ignore") as r:
+with open("wikipedia_featured_articles.txt", "r", encoding="utf-8", errors="ignore") as r:
     with codecs.open("TopicList.txt", "w", encoding="utf-8") as w: # utf-8で書き込む
         content = r.readlines()
         for line in content:
+            # 行頭が=のとき、トピックに関する処理を行う
             if line[0] == "=":
                 line = line.strip("=")
                 line = line.split("=")
@@ -20,8 +21,8 @@ with open("wiki_feature_articles.txt", "r", encoding="utf-8", errors="ignore") a
                 fileName = "Topic/" + line + ".txt"
                 print(fileName)
                 f = codecs.open(fileName, "w", encoding='utf-8')
-
-            elif line[:21] == "* {{FA/BeenOnMainPage":
+            # 記事に対する処理
+            elif line[0:19] == "{{FA/BeenOnMainPage" or line[0:2] == "[[":
                 line = line.split("[[")
                 line = line[1].split("]]")
                 f.write(line[0] + "\n")
